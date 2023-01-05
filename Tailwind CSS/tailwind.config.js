bb0f9fda2da150d80be00b3ed75263184634c198
sethonne <20103261@usc.edu.ph>
2023-01-05 23:05:39 +0800

    initial commit

--- Tailwind CSS/tailwind.config.js
+++ Tailwind CSS/tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        primary: "#FF6363",
-        secondary: {
-          100: "#E2E2D5",
-          200: "#888883",
-        },
-      },
+      // colors: {
+      //   primary: "#FF6363",
+      //   secondary: {
+      //     100: "#E2E2D5",
+      //     200: "#888883",
+      //   },
+      // },
     },
   },
   plugins: [],
